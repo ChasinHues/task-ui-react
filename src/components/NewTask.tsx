3fc201245ba1b9c2f91afd8709eaf2f270ad1f56
ChasinHues <jasonhughes1@outlook.com>
2020-07-06 22:00:49 +1000

    Minor UI update

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -20,9 +20,17 @@ export default class NewTask extends React.Component<INewTaskProps, any> {
 
     render() {
         return (
-            <div className="fixed bottom-0 right-0 mr-5 border border-gray-500 border-b-0 rounded-t p-2">
-                <input value={this.props.value} type="text" onChange={this.props.changeHandler} onKeyPress={this.handleAddNewTask} />
-                <button onClick={this.props.addNewTaskHandler}>Add</button>
+            <div className="fixed bottom-0 right-0 mr-5 border border-gray-500 border-b-0 rounded-t p-2 flex items-center">
+                <input value={this.props.value} type="text" 
+                    onChange={this.props.changeHandler} 
+                    onKeyPress={this.handleAddNewTask}
+                    className="border rounded p-1 " />
+                <button 
+                    onClick={this.props.addNewTaskHandler}
+                    className="border p-1 ml-2 rounded-full w-6 h-6 flex items-center border-green-500 hover:bg-green-500 text-green-500 hover:text-white"
+                    >
+                        <i className="fas fa-plus pointer-events-none"></i>
+                    </button>
             </div>
         )
     }
